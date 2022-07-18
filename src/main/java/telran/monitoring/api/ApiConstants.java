50f4b70a18f0210d004290e663939bd24e2a891d
Yuragranovsky <yuragranovsky@gmail.com>
2022-07-18 12:40:11 +0300

    adds PULSE and VISITS constants

--- src/main/java/telran/monitoring/api/ApiConstants.java
+++ src/main/java/telran/monitoring/api/ApiConstants.java
@@ -5,4 +5,5 @@ String DOCTOR_PATIENT_PROVIDER_URL = "/doctor/patient/";
 String ADD_PATIENT = "/patients";
 String ADD_DOCTOR = "/doctors";
 String VISITS = "/visits";
+String PULSE_VALUES = "/pulse/values/";
 }
